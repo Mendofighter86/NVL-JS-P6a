d90e04007022ade45b14bcfb6090368f9acf3ff9
Mendofighter86 <61906372+Mendofighter86@users.noreply.github.com>
2020-04-25 22:24:47 +0200

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,5 @@
 var nombre = "Carlos";
+var apellidos = "Mendoza Pérez";
 var edad = 33;
 var diasSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 var gravedad= "9.8 m^2/s";
